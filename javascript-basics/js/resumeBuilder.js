55483f2d47997d72a7ec07d8921a346c024b9d17
Caue Queiroz <cauenqueiroz@gmail.com>
2016-11-04 19:58:58 -0200

    Create personal information objects

--- javascript-basics/js/resumeBuilder.js
+++ javascript-basics/js/resumeBuilder.js
@@ -1,3 +1,87 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+    ========================================
+    Personal Info    
+    ========================================
+*/
+
+var bio = {
+    "name": "Caue Queiroz",
+    "role": "Front-end Developer",
+    "welcomeMessage": "I am the one who knocks.",
+    "biopic": "images/fry.jpg",
+    "contacts": {
+        "mobile": "55 11 946969442",
+        "email": "cauenqueiroz@gmail.com",
+        "github": "cauequeiroz",
+        "twitter": "cauequeirozz",
+        "location": "São Paulo SP Brazil"
+    },
+    "skills": ["HTML", "CSS", "Javascript"]
+};
+
+var projects = [
+    {
+        "title": "Genius Dev Edition",
+        "dates": "2017",
+        "description": "A genius version for developers.",
+        "images": ["images/fry.jpg", "images/fry.jpg"]
+    },
+    {
+        "title": "TicTacToe",
+        "dates": "2016",
+        "description": "Tictactoe game built with JS.",
+        "images": ["images/fry.jpg", "images/fry.jpg"]
+    }
+];
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Intuitive Intelligence",
+            "title": "Front-end Developer",
+            "location": "São Paulo SP Brazil",
+            "dates": "2005 ~ 2009",
+            "description": "Develop some great things with JS" 
+        },
+        {
+            "employer": "Intuitive Intelligence",
+            "title": "Front-end Developer",
+            "location": "São Paulo SP Brazil",
+            "dates": "2005 ~ 2009",
+            "description": "Develop some great things with JS" 
+        },
+        {
+            "employer": "Intuitive Intelligence",
+            "title": "Front-end Developer",
+            "location": "São Paulo SP Brazil",
+            "dates": "2005 ~ 2009",
+            "description": "Develop some great things with JS" 
+        }
+    ]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "USP",
+            "location": "São Paulo SP Brazil",
+            "dates": "2010 ~ 2014",
+            "url": "http://usp.com.br/",
+            "majors": ["CompScience", "English"]
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Intro to AJAX",
+            "school": "Udacity",
+            "dates": "2015",
+            "url": "http://udacity.com/ajax"
+        },
+        {
+            "title": "Javascript Design Patterns",
+            "school": "Udacity",
+            "dates": "2016",
+            "url": "http://udacity.com/design"
+        }
+    ]
+}
