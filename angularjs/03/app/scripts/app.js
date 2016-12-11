6b3e3d3c15755773881ba808d6b5efb51c8db367
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-11 01:11:33 -0200

    Start third exercise

---
+++ angularjs/03/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udaciMealsApp
+ * @description
+ * # udaciMealsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udaciMealsApp', []);
