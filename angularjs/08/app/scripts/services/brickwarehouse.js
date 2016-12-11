d4a246bb68bc3838b776b723e34756bf31d1cc3c
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-11 06:26:26 -0200

    Finish exercise 08

---
+++ angularjs/08/app/scripts/services/brickwarehouse.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name serviceQuizApp.brickWarehouse
+ * @description
+ * # brickWarehouse
+ * Service in the serviceQuizApp.
+ */
+angular.module('serviceQuizApp')
+  .service('brickWarehouse', function () {
+    this.bricks = {
+    	'red': {
+            '1x1': {
+            quantity: 13,
+            price: 0.01
+            },
+            '2x2': {
+            quantity: 48,
+            price: 0.03
+            },
+            '2x6': {
+            quantity: 9,
+            price: 0.05
+            }
+    	},
+        'blue': {
+            '2x2': {
+                quantity: 7,
+                price: 0.02
+              },
+              '2x4': {
+                quantity: 2,
+                price: 0.04
+              }
+          },
+          'green': {
+            '2x4': {
+            quantity: 13,
+            price: 0.04
+          },
+          '2x8': {
+            quantity: 41,
+            price: 0.08
+          }
+          }
+    };
+  });
