e8bf19f47ba99c4818b06689bcca7bf78883105a
Caue Queiroz <cauenqueiroz@gmail.com>
2016-12-15 15:10:20 -0200

    Remove old ember folder

--- ember/01/public/grader/grader.js
+++
@@ -1,7 +0,0 @@
-QUnit.done(function( details ) {
-
-  if (details.passed === details.total && details.failed === 0) {
-    window.dispatchEvent(new CustomEvent('tests-pass', {'detail': 'passed'}));
-  }
-
-});
