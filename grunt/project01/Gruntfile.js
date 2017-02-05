5d626b25902ac0ff6bf0f64eb22bc7d1a531afeb
Caue Queiroz <cauenqueiroz@gmail.com>
2017-02-05 14:37:12 -0200

    update grunt project

--- grunt/project01/Gruntfile.js
+++ grunt/project01/Gruntfile.js
@@ -8,11 +8,19 @@ module.exports = function(grunt) {
                 src: 'js/*.js',
                 dest: 'build/production.js'
             }
+        },
+
+        uglify: {
+            build: {
+                src: 'build/production.js',
+                dest: 'build/production.min.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 
 };
